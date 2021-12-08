e536d9c4dbb4ba21da2ad79a5ab66a7d7ac7b94b
Bobby Nugraha Ariffin <bobzzee@gmail.com>
2021-12-08 17:40:16 +0700

    looping4

--- looping4.php
+++ looping4.php
@@ -6,12 +6,62 @@
 // ****
 // *****
 
-
 $bintang = '*';
 $text = '';
 
-for ($i = 0; $i < 5; $i++) {
-    // kode
+// print_r($text . $bintang);
+// die;
+
+// for ($i = 0; $i < 5; $i++) {
+//     $text = $text . $bintang;
+//     print_r($text);
+//     echo '<br>';
+// }
+
+// for ($i = 5; $i > 0; $i--) {
+//     $text = str_repeat($bintang, $i);
+//     print_r($text);
+//     echo '<br>';
+// }
+
+// $bintang = '*';
+
+// for ($i = 5; $i > 0; $i--) {
+//     $text = '';
+
+//     for ($j = 0; $j < $i; $j++) {
+//         $text .= $bintang;
+//     }
+
+//     print_r($text);
+//     echo '<br>';
+// }
+
+$star = '*';
+$spacer = '_';
+$line = 5;
+
+// 5  0  9
+// 4  1  7
+// 3  2  5
+// 2  3  3
+// 1  4  1
+
+for ($i = $line; $i > 0; $i--) {
+    $text = '';
+
+    for ($j = 0; $j < ($line - $i); $j++) {
+        $text = $text . $spacer;
+    }
+
+    for ($j = 0; $j < ($i * 2 - 1); $j++) {
+        $text = $text . $star;
+    }
+
+    for ($j = 0; $j < ($line - $i); $j++) {
+        $text = $text . $spacer;
+    }
+
     print_r($text);
     echo '<br>';
 }
