18661264e96dddb8513cdb6e580f31be9485dba4
Bobby Nugraha Ariffin <bobzzee@gmail.com>
2021-12-08 16:45:23 +0700

    dor

--- looping3.php
+++ looping3.php
@@ -1,7 +1,7 @@
 <?php
 
-var_dump(1 % 2);
-die;
+// var_dump(1 % 2);
+// die;
 
 $array = [
     'item 1',
@@ -10,10 +10,12 @@ $array = [
     'item 4',
     'item 5',
     'item 6',
+    'item 7',
 ];
 
 foreach ($array as $key => $value) {
-
-    print_r("{$key} - {$value}");
-    echo '<br>';
+    if ($key % 2 === 1) {
+        print_r($value);
+        echo '<br>';
+    }
 }
