2d32c1902dd4b86a6f125f3ed7af47b552ee72e3
Bobby Nugraha Ariffin <bobzzee@gmail.com>
2021-12-08 17:48:26 +0700

    update

--- looping4.php
+++ looping4.php
@@ -41,23 +41,30 @@ $star = '*';
 $spacer = '_';
 $line = 5;
 
-// 5  0  9
-// 4  1  7
-// 3  2  5
-// 2  3  3
-// 1  4  1
+// space    star    space  
+//   0       9        0
+//   1       7        1
+//   2       5        2
+//   3       3        3
+//   4       1        4
+
+// space  = line - i
+// star   = line * 2 - 1
 
 for ($i = $line; $i > 0; $i--) {
     $text = '';
 
+    // spacer kiri
     for ($j = 0; $j < ($line - $i); $j++) {
         $text = $text . $spacer;
     }
 
+    // bintang
     for ($j = 0; $j < ($i * 2 - 1); $j++) {
         $text = $text . $star;
     }
 
+    // spacer kanan
     for ($j = 0; $j < ($line - $i); $j++) {
         $text = $text . $spacer;
     }
