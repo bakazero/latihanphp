d3c90d7ded283d82c8e325c90182aa7546ac6a79
Bobby Nugraha Ariffin <bobzzee@gmail.com>
2021-12-27 15:38:19 +0700

    dor

---
+++ 211227_senin/sql_injection.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once '../function.php';
+
+$db = dbConnection();
+
+$test = '3333 OR 1=1';
+
+// contoh sql injection
+
+$sql = "SELECT * FROM account WHERE id = {$test}";
+$stmt = $db->query($sql);
+
+$inject = $stmt->fetch(PDO::FETCH_ASSOC);
+
+// contoh pengamanan dengan data binding
+
+$sql = "SELECT * FROM account WHERE id = ?";
+$stmt = $db->prepare($sql);
+$stmt->execute([
+    $test
+]);
+
+$binding = $stmt->fetch(PDO::FETCH_ASSOC);
+
+print_r([
+    'inject' => $inject,
+    'binding' => $binding
+]);
